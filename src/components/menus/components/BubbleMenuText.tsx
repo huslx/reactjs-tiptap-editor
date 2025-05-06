bf0c0f175b8582f6b82d82e5f25e4b3f49bc4973
lilyzhou <lj_zhou@outlook.com>
2025-05-06 21:00:48 +0800

    chore: 0.2.38

--- src/components/menus/components/BubbleMenuText.tsx
+++ src/components/menus/components/BubbleMenuText.tsx
@@ -16,12 +16,6 @@ export interface BubbleMenuTextProps {
   showShow?: (props: ShouldShowProps) => boolean
 }
 
-const tippyOptions = {
-  maxWidth: 'auto',
-  zIndex: 20,
-  moveTransition: 'transform 0.1s ease-out',
-};
-
 export function ItemA({ item, disabled, editor }: any) {
   const Comp = item.component;
 
@@ -41,7 +35,8 @@ export function ItemA({ item, disabled, editor }: any) {
 function BubbleMenuText(props: BubbleMenuTextProps) {
   const { t, lang } = useLocale();
 
-  const shouldShow = ({ editor }: ShouldShowProps) => {
+  const shouldShow = (args: ShouldShowProps) => {
+    const { editor } = args;
     const { selection } = editor.view.state;
     const { $from, to } = selection;
 
@@ -50,7 +45,7 @@ function BubbleMenuText(props: BubbleMenuTextProps) {
       return false;
     }
 
-    return selection instanceof TextSelection;
+    return selection instanceof TextSelection || (props.showShow?.(args) ?? false);
   };
 
   const items = useMemo(() => {
@@ -63,40 +58,44 @@ function BubbleMenuText(props: BubbleMenuTextProps) {
   }, [props.disabled, props.editor, props.items, props.filterItem, lang, t]);
 
   return (
-    <BubbleMenu editor={props?.editor}
-      shouldShow={props.showShow ?? shouldShow}
-      tippyOptions={tippyOptions as any}
+    <BubbleMenu
+      editor={props?.editor}
+      shouldShow={shouldShow}
+      tippyOptions={{
+        maxWidth: 'auto',
+        zIndex: 20,
+        moveTransition: 'transform 0.1s ease-out',
+        placement: 'bottom'
+      }}
     >
-      {items?.length
-        ? (
-          <div className="pointer-events-auto w-auto select-none rounded-sm !border border-neutral-200 bg-background px-3 py-2 shadow-sm transition-all dark:border-neutral-800">
-            <div className="relative flex h-[26px] flex-nowrap items-center justify-start gap-[4px] whitespace-nowrap">
-              {items?.map((item: any, key: any) => {
-                if (item?.type === 'divider') {
-                  return (
-                    <Separator
-                      className="!mx-1 !my-2 !h-[16px]"
-                      key={`bubbleMenu-divider-${key}`}
-                      orientation="vertical"
-                    />
-                  );
-                }
-
+      {items?.length ? (
+        <div className="pointer-events-auto w-auto select-none rounded-sm !border border-neutral-200 bg-background px-3 py-2 shadow-sm transition-all dark:border-neutral-800">
+          <div className="relative flex h-[26px] flex-nowrap items-center justify-start gap-[4px] whitespace-nowrap">
+            {items?.map((item: any, key: any) => {
+              if (item?.type === 'divider') {
                 return (
-                  <ItemA
-                    disabled={props.disabled}
-                    editor={props.editor}
-                    item={item}
-                    key={`bubbleMenu-text-${key}`}
+                  <Separator
+                    className="!mx-1 !my-2 !h-[16px]"
+                    key={`bubbleMenu-divider-${key}`}
+                    orientation="vertical"
                   />
                 );
-              })}
-            </div>
+              }
+
+              return (
+                <ItemA
+                  disabled={props.disabled}
+                  editor={props.editor}
+                  item={item}
+                  key={`bubbleMenu-text-${key}`}
+                />
+              );
+            })}
           </div>
-        )
-        : (
-          <></>
-        )}
+        </div>
+      ) : (
+        <></>
+      )}
     </BubbleMenu>
   );
 }
